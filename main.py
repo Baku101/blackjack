f99aff959accd829b95dc55d8d168f91e329b411
LapszanskiZED <115982866+LapszanskiZED@users.noreply.github.com>
2023-06-12 22:21:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#sprawdzy czy dziala es
+import pygame
+from definicja_klas import *
+import time
+window = pygame.display.set_mode((800, 600)) #tworzy okno
+pygame.font.init()
+
+def level_one(iloscTali):
+  talia=Talia(iloscTali)
+    PrzyciskPasu=True
+    punktyZAsemGracza=0
+    punktyZAsemBrukiera=0
+    run=True
+    clock=0
+    punkty=0
+    punktyBrukiera=0
+    kartygracza=0
+    punktyGracza=0
+    losuj = True
+    tablicaObiektow=[]
+    tablicaObiektowBrukier=[]
+    przyciskDoboruBool=True
+    last_created_time = time.time()
+    iloscpostaci=0
+    start_time = pygame.time.get_ticks()
