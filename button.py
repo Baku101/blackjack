7a4b34580a2a5a2dbcc4f054a47186fed90d5e30
Baku101 <128596106+Baku101@users.noreply.github.com>
2023-06-12 23:02:20 +0200

    liczba talii oraz ustawienie tła

--- button.py
+++ button.py
@@ -14,3 +14,12 @@ pygame.display.set_caption("Blackjack")
 #rozmiar przycisku i suwaka
 slider = Slider(win, 75, 50, 150, 20, min=1, max=8, step=1)
 output = Button(win, 110, 100, 80, 30,text="", fontSize=8,onClick=lambda: print(slider.getValue()))
+
+#liczba talii
+liczba_talii = slider.getValue()
+
+#ustawienie zdjęcia jako tła
+current_dir = os.path.dirname(__file__)
+background_path = os.path.join(current_dir, "bgg.jpg")
+background_image = pygame.image.load(background_path)
+background_image = pygame.transform.scale(background_image, (300, 170))
