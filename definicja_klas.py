331f1483dcf2c26b8b9884537acd2fe185ffdea6
Baku101 <128596106+Baku101@users.noreply.github.com>
2023-06-12 22:11:01 +0200

    Update definicja_klas.py (def odswiez)

--- definicja_klas.py
+++ definicja_klas.py
@@ -11,3 +11,11 @@ import time
 current_time = time.time()
 resolution = (800, 600)
 last_created_time = time.time()
+
+def odswiez(punktyGracza, punktyBrukiera, window):
+    ilosc_punktow_aktualna = pygame.font.Font.render(pygame.font.SysFont("arial", 200), str(punktyGracza), True,
+                                                     (0, 0, 0))
+    ilosc_punktow_aktualna_brukiera = pygame.font.Font.render(pygame.font.SysFont("arial", 200), str(punktyBrukiera),
+                                                              True, (0, 0, 0))
+    window.blit(ilosc_punktow_aktualna_brukiera, (0, 0))
+    window.blit(ilosc_punktow_aktualna, (0, 400))
