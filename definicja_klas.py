1e84502265a13d0be5818a764d55457131821980
ArthasToZiom <128599025+ArthasToZiom@users.noreply.github.com>
2023-06-12 22:18:45 +0200

    Def przegrałeś, remis, wygrałeś.

--- definicja_klas.py
+++ definicja_klas.py
@@ -27,3 +27,56 @@ class Button:
         self.button_image = pygame.image.load(zdj)
         self.hovered_button_image = pygame.image.load(zdj1)
         self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.button_image.get_width(), self.button_image.get_he
+ 
+ def wygrales():
+
+    time.sleep(2.5)
+    wygrana=pygame.image.load("D:\karty\ptlowybranej.png")
+    window.blit(wygrana, (-100, 0))
+    koniec = pygame.font.Font.render(pygame.font.SysFont("arial", 170), "WYGRALES", True, (0, 0, 0))
+    window.blit(koniec, (0, 400))
+    pygame.display.update()
+    time.sleep(5)
+
+def remis():
+    time.sleep(2.5)
+    remis = pygame.image.load("D:\karty\wremis.jpg")
+    window.blit(remis, (0, 0))
+    koniec = pygame.font.Font.render(pygame.font.SysFont("arial", 170), "remis", True, (0, 0, 0))
+    window.blit(koniec, (0, 0))
+    pygame.display.update()
+    time.sleep(5)
+
+def przegrales(punktyGracza, punktyBrukiera):
+    time.sleep(2)
+    tlo_gra = pygame.image.load("D:\Beznazwy.png")
+    window.blit(tlo_gra, (0, 0))
+    matrix = pygame.image.load("D:\karty\poop.png")
+    window.blit(matrix, (-250, 0))
+    if(punktyGracza>21):
+
+        koniec = pygame.font.Font.render(pygame.font.SysFont("arial", 170), "przegrales", True,
+                                     (0, 0, 0))
+
+        koniecPunktyGracza = pygame.font.Font.render(pygame.font.SysFont("arial", 70), "twoje punkty: "+str(punktyGracza), True, (100, 0, 0))
+        window.blit(koniecPunktyGracza, (300, 300))
+    else:
+        koniec = pygame.font.Font.render(pygame.font.SysFont("arial", 170), "przegrales", True,
+                                         (0, 0, 0))
+
+        koniecPunktyGracza = pygame.font.Font.render(pygame.font.SysFont("arial", 70),
+                                                     "twoje punkty: " + str(punktyGracza), True, (100, 0, 0))
+        window.blit(koniecPunktyGracza, (300, 300))
+        koniecPunktyBrukiera=pygame.font.Font.render(pygame.font.SysFont("arial", 70), "punkty Brukiera: "+str(punktyBrukiera), True, (100, 0, 0))
+        window.blit(koniecPunktyBrukiera, (300, 400))
+
+
+    print("przegrales")
+    window.blit(koniec, (0, 400))
+    pygame.display.update()
+    time.sleep(5)
+def wkzdj(kolor, liczba):
+
+    nazwa_pliku = f"D:\karty\{liczba}{kolor}.png"
+
+    return nazwa_pliku
