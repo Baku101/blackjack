ae7cd281a9f9ca248521a91d7cbce52382657d5f
Kacper Pelczarski <127994739+KacperP0@users.noreply.github.com>
2023-06-12 22:16:16 +0200

    Początek klasy Button

--- definicja_klas.py
+++ definicja_klas.py
@@ -19,3 +19,11 @@ def odswiez(punktyGracza, punktyBrukiera, window):
                                                               True, (0, 0, 0))
     window.blit(ilosc_punktow_aktualna_brukiera, (0, 0))
     window.blit(ilosc_punktow_aktualna, (0, 400))
+    
+    class Button:
+    def __init__(self, x_cord, y_cord, zdj, zdj1):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.button_image = pygame.image.load(zdj)
+        self.hovered_button_image = pygame.image.load(zdj1)
+        self.hitbox = pygame.Rect(self.x_cord, self.y_cord, self.button_image.get_width(), self.button_image.get_he
