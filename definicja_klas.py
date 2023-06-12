d2c7037c6d75596a0758325d3bb3904f358b2e0b
LapszanskiZED <115982866+LapszanskiZED@users.noreply.github.com>
2023-06-12 22:10:30 +0200

    Update definicja_klas.py

--- definicja_klas.py
+++ definicja_klas.py
@@ -1,2 +1,13 @@
-#test
-#testtest
+from main import*
+from level_one import *
+
+
+pygame.init() # innicjowanie okna
+window = pygame.display.set_mode((800, 600))
+import pygame
+
+import random
+import time
+current_time = time.time()
+resolution = (800, 600)
+last_created_time = time.time()
