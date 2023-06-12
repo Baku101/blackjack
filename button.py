b0f31a01bb8d2c32e0d68470063925e4effae6ab
Baku101 <128596106+Baku101@users.noreply.github.com>
2023-06-12 23:01:11 +0200

    stworzenie pliku, dodanie bibliotek oraz ustawienie rozmiaru okna

---
+++ button.py
@@ -0,0 +1,9 @@
+import pygame_widgets
+import pygame
+import os
+from pygame_widgets.slider import Slider
+from pygame_widgets.button import Button
+
+pygame.init()
+#rozmiar okna
+win = pygame.display.set_mode((300, 170))
