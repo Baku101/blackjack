a9026c3642cbca76bad74bc5df7c673a3a7e18a6
Baku101 <128596106+Baku101@users.noreply.github.com>
2023-06-12 23:01:48 +0200

    ustawienie tytułu okna oraz rozmiaru przycisku i suwaka

--- button.py
+++ button.py
@@ -7,3 +7,10 @@ from pygame_widgets.button import Button
 pygame.init()
 #rozmiar okna
 win = pygame.display.set_mode((300, 170))
+
+# Ustawienie tytułu okna
+pygame.display.set_caption("Blackjack")
+
+#rozmiar przycisku i suwaka
+slider = Slider(win, 75, 50, 150, 20, min=1, max=8, step=1)
+output = Button(win, 110, 100, 80, 30,text="", fontSize=8,onClick=lambda: print(slider.getValue()))
