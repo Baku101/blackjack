cdd8de11fc1a54ae06c44591c34b705998dd8666
ArthasToZiom <128599025+ArthasToZiom@users.noreply.github.com>
2023-06-12 22:31:17 +0200

    Liczba punktów gracza

--- main.py
+++ main.py
@@ -35,3 +35,12 @@ def level_one(iloscTali):
         keys = pygame.key.get_pressed()
         window.fill((181,11,11))
         window.blit(tlo_gra, (0, 0))
+        ilosc_punktow_aktualna = pygame.font.Font.render(pygame.font.SysFont("arial", 200), str(punktyGracza), True,
+                                                         (0, 0, 0))
+        ilosc_punktow_aktualna_brukiera = pygame.font.Font.render(pygame.font.SysFont("arial", 200),str(punktyBrukiera), True, (0, 0, 0))
+
+        window.blit(ilosc_punktow_aktualna_brukiera, (0, 0))
+
+
+
+        window.blit(ilosc_punktow_aktualna, (0, 400))
