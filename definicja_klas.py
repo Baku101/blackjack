59746ffdbb1ccc31ba8defc7934ff8ec1616fc97
ArthasToZiom <128599025+ArthasToZiom@users.noreply.github.com>
2023-06-12 22:38:59 +0200

    Odczyt wartości z kart

--- definicja_klas.py
+++ definicja_klas.py
@@ -12,6 +12,28 @@ current_time = time.time()
 resolution = (800, 600)
 last_created_time = time.time()
 
+def get_wartosc(number):
+    slownik = {
+        1:1,
+        2:2,
+        3:3,
+        4:4,
+        5:5,
+        6:6,
+        7:7,
+        8:8,
+        9:9,
+        10:10,
+        11:10,
+        12:10,
+        13:10,
+        14:1
+    }
+    if number in slownik:
+        return slownik[number]
+    else:
+        print("cos cie swedzi")
+
 def odswiez(punktyGracza, punktyBrukiera, window):
     ilosc_punktow_aktualna = pygame.font.Font.render(pygame.font.SysFont("arial", 200), str(punktyGracza), True,
                                                      (0, 0, 0))
