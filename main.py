edb8593be91c08a3c601af4c905df71c1963dc87
Kacper Pelczarski <127994739+KacperP0@users.noreply.github.com>
2023-06-13 23:51:38 +0200

    warunek gdy punkty brukiera przekraczają 21

--- main.py
+++ main.py
@@ -291,7 +291,19 @@ if( PrzyciskPasu==True and len(tablicaObiektow)>=4):
                             if x == 1:
                                 run = False
 
-                            return 1                    
+                            return 1
+                    if(punktyBrukiera>21):
+                        ilosc_punktow_aktualna = pygame.font.Font.render(pygame.font.SysFont("arial", 200),
+                                                                         str(punktyGracza), True,
+                                                                         (0, 0, 0))
+                        ilosc_punktow_aktualna_brukiera = pygame.font.Font.render(pygame.font.SysFont("arial", 200),
+                                                                                  str(punktyBrukiera), True, (0, 0, 0))
+                        window.blit(ilosc_punktow_aktualna_brukiera, (0, 0))
+                        window.blit(ilosc_punktow_aktualna, (0, 400))
+                        x = wygrales()
+                        if x == 5:
+                            run = False
+                        return 5
 def main(): #to jest menu
 
     wygrana=0
